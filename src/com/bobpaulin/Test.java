ed697fced4e31c8735cc6a52561e72091730912d
bpaulin <bpaulin@Bob_Paulin>
2014-03-12 06:27:01 -0500

    Initial Commit Ivy Test

---
+++ src/com/bobpaulin/Test.java
@@ -0,0 +1,13 @@
+package com.bobpaulin;
+
+import org.apache.commons.lang.StringUtils;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		StringUtils.isAlpha(args[0]);
+
+	}
+
+}
